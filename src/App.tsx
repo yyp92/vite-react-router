0339713f819f21d5a289accbed5e7e6c527e4786
yyp <yangyipeng@joyame.com>
2024-04-26 18:49:25 +0800

    init

---
+++ src/App.tsx
@@ -0,0 +1,86 @@
+import { useState } from 'react';
+import {
+    Link,
+    useLocation,
+    RouterProvider,
+    createBrowserRouter,
+    Outlet,
+    useOutlet
+} from 'react-router-dom';
+
+const Layout = () => {
+    const { pathname } = useLocation();
+    const element = useOutlet()
+
+    return (
+        <div>
+            <div>当前路由: {pathname}</div>
+            
+            {/* <Outlet/> */}
+
+            {element}
+        </div>
+    )
+}
+
+const Aaa = () => {
+    const [count, setCount] = useState(0);
+
+    return <div>
+        <p>{count}</p>
+
+        <p>
+            <button onClick={() => setCount(count => count + 1)}>加一</button>
+        </p>
+
+        <Link to='/bbb'>去 Bbb 页面</Link><br/>
+        <Link to='/ccc'>去 Ccc 页面</Link>
+    </div>
+};
+
+const Bbb = () => {
+    const [count, setCount] = useState(0);
+
+    return <div>
+        <p>{count}</p>
+        <p><button onClick={() => setCount(count => count + 1)}>加一</button></p>
+        <Link to='/'>去首页</Link>
+    </div>
+};
+
+const Ccc = () => {
+    return <div>
+        <p>ccc</p>
+        <Link to='/'>去首面</Link>
+    </div>
+};
+
+const routes = [
+    {
+        path: "/",
+        element: <Layout></Layout>,
+        children: [
+            {
+                path: "/",
+                element: <Aaa></Aaa>,
+            },
+            {
+                path: "/bbb",
+                element: <Bbb></Bbb>
+            },
+            {
+                path: "/ccc",
+                element: <Ccc></Ccc>
+            }
+        ]
+    }
+];
+
+export const router = createBrowserRouter(routes);
+
+const App = () => {
+    return <RouterProvider router={router}/>
+}
+
+export default App;
+
